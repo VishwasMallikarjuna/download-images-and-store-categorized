e3f46fb7b23ea94584faba059327c4550c642911
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-04-08 00:13:36 +0530

    update main.go

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 const (
 	breedsEndpoint   = "https://dog.ceo/api/breeds/list/all"
 	breedImageFormat = "https://dog.ceo/api/breed/%s/images/random"
@@ -11,4 +16,20 @@ type BreedsResponse struct {
 
 func main() {
 	breeds, err := getAllBreeds()
+}
+
+func getAllBreeds() (map[string][]string, error) {
+	resp, err := http.Get(breedsEndpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var breedsResponse BreedsResponse
+	err = json.NewDecoder(resp.Body).Decode(&breedsResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return breedsResponse.Message, nil
 }
